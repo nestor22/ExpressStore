7627a692062c1d6102057501866b4184df61ddc3
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-05-24 20:53:20 -0600

    Servidor inicial de express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("mi app")
+const express = require('express');
+
+const app = express();
+
+const port = 3001;
+
+app.get('/', (req, res)=>{
+  res.send('hello world');
+})
+
+app.listen(port, ()=>{
+  console.log('mi port is ', port)
+})
