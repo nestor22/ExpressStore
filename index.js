4f2ecf49787635fb0abd0ea62b7321a506a6ae06
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-05-28 18:00:57 -0600

    [ADD] query routes and params

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const faker = require('faker');
+
 const req = require('express/lib/request');
 
 const app = express();
@@ -13,32 +15,44 @@ app.get('/nuva-ruta', (res) => {
   res.send('soy otra rutas');
 });
 
-app.get('/products', (res) => {
-  res.json([
-    { name: 'producto 1', price: 1000 },
-    { name: 'producto 1', price: 1000 },
-  ]);
+app.get('/products', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index< limit; index++) {
+    products.push({ 
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(), 
+    });
+  }
+  res.json(products);
 });
+
+app.get('/products/filter', function (req, res) {
+  res.send.apply('shoy un filter');
+})
+
+
 
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
   res.json({ name: 'producto 1', price: 1000 });
 });
 
-
-app.get('/categories/:categoryId/products/:productId',(req, res)=>{
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
   const { categoryId, productId } = req.params;
-  res.json({categoryId, productId});
-})
+  res.json({ categoryId, productId });
+});
 
-app.get('/users', (req, res)=>{
+app.get('/users', (req, res) => {
   const { limit, offset } = req.query;
-  if(limit && offset){
-    res.json({limit, offset});
-  }else{
+  if (limit && offset) {
+    res.json({ limit, offset });
+  } else {
     res.send(404, 'no parametros');
   }
-})
+});
 
 app.listen(port, () => {
   console.log('mi port is ', port);
